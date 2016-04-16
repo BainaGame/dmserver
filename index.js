2f77c4f4173f26120cfdf475dc6435b6646e009a
wyang <wyang@bainainfo.com>
2016-04-16 17:49:53 +0800

    dm服务器

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var bodyParser = require("body-parser");
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.use(bodyParser.urlencoded({extended: false}));
+
+//get/post 访问方法一样
+app.use(function(req,res,next){
+    if(req.method == "GET"){
+        req.body = req.query;
+    }
+    next();
+})
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+app.get('/client.html', function (req, res) {
+    console.log("readfile:client.html")
+    res.sendFile(__dirname + '/client.html');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+app.get('/socket.io-1.2.0.js', function (req, res) {
+    res.sendFile(__dirname + '/socket.io-1.2.0.js');
+});
+
+app.get('/sendmsg', function (req, res) {
+
+    var msg = req.body.msg;
+    io.emit('dm', msg);
+    res.send("success");
+})
+
+io.on('connection', function (socket) {
+    socket.on('dm', function (msg) {
+        io.emit('dm', msg);
+    });
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
 });
