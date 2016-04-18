e6d8dd24abbe378470d924e63fd8ef791531d77a
wyang <wyang@bainainfo.com>
2016-04-18 10:39:57 +0800

    增加微信验证

--- index.js
+++ index.js
@@ -31,6 +31,11 @@ app.get('/socket.io-1.2.0.js', function (req, res) {
     res.sendFile(__dirname + '/socket.io-1.2.0.js');
 });
 
+///baina?signature=64fae32d0580de84aa45f9aeaca6f1025f56eb93&echostr=8145551735383739278&timestamp=1460947078&nonce=441269505
+app.get("/baina", function (req,res){
+    return true
+})
+
 app.get('/sendmsg', function (req, res) {
 
     var msg = req.body.msg;
